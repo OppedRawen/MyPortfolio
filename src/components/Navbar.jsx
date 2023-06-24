28c733858f5e603016afc74a4b58726628bfce03
OppedRawen <yud2372@gmail.com>
2023-06-23 21:26:56 -0700

    for building purposes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {motion} from 'framer-motion';
 import {HiOutlineDesktopComputer} from 'react-icons/hi';
-import {FaComputer} from 'react-icons/fa';
+
 const Navbar = () => {
 
   const handleLinkClick = (e, sectionId) => {
@@ -13,23 +13,7 @@ const Navbar = () => {
   };
 
     return(
-  //       <nav className=' py-10 mb-12 flex justify-between text-[#cacaca]'>
-  //   <h1 className=' text-lg font-burtons font-semibold'>Portfolio</h1>
-  //           <div className="flex justify-end text-sm">
-           
-  //       <ul className=' flex items-center'>
-  //           <li className=' ml-6 mr-6 font-semibold hover:text-[#54d5bb]'><motion.a href="#about" whileTap={{ scale: 0.95 }}
-  // onClick={(e) => handleLinkClick(e, "about")}>About Me</motion.a></li>
-  //           <li className=' ml-6 mr-6 font-semibold hover:text-[#54d5bb]'><motion.a href="#experience" whileTap={{ scale: 0.95 }}
-  // onClick={(e) => handleLinkClick(e, "experience")>Experience</motion.a></li>
-  //           <li className=' ml-6 mr-6 font-semibold hover:text-[#54d5bb]'><a href="#projects">Projects</a></li>
-  //           <li className=' ml-6 mr-6 font-semibold hover:text-[#54d5bb]'><a href="#contact">Contact</a></li>
-           
-  //           <li><a className=' bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8 ' href='https://drive.google.com/file/d/1NQh2d7mnP6IkLeX13L5u4RGLhtw_9ClD/view?usp=drive_link'>Resume</a></li>
-  //         </ul>
-  //           </div>
-       
-  //         </nav>
+
   <nav className="py-10 mb-12 flex justify-between text-[#cacaca]">
       {/* <h1 className="text-lg font-burtons font-semibold">Portfolio</h1> */}
       <HiOutlineDesktopComputer className="text-3xl font-bold w-10 h-10"/>
