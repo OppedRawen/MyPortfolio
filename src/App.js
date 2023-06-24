46cd98f1a7887a22ed6d9f6885a609b6310498e5
OppedRawen <yud2372@gmail.com>
2023-06-23 23:45:54 -0700

    loading bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import './App.css';
 
@@ -8,26 +8,34 @@ import Footer from "./footer";
 import {Contact,Navbar,Experience,Projects,About,Introduction,Loading} from './components';
 
 function App() {
+const [isLoading,setIsLoading]=useState(true);
+useEffect(() => {
+  // Simulating a 1-second delay before loading the main page
+  const timer = setTimeout(() => {
+    setIsLoading(false);
+  }, 1000);
 
+  return () => clearTimeout(timer); // Clear the timer when the component is unmounted
+}, []);
   return (
     <div className=" bg-[#1d1d20] px-10">
-      <header className=" min-h-screen">
-       <Loading />
-        <Navbar />
-       
-        <About />
-        
-      </header>
-      <Introduction />
-      <Skills />
-
-     <Experience />
-
-      
-      <Projects />
+    {isLoading ? (
+      <Loading />
+    ) : (
+      <>
+        <header className=" min-h-screen">
+          <Navbar />
+          <About />
+        </header>
+        <Introduction />
+        <Skills />
+        <Experience />
+        <Projects />
         <Contact />
-      <Footer />
-    </div>
+        <Footer />
+      </>
+    )}
+  </div>
   );
 }
 
