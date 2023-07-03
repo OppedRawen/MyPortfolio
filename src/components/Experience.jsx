7af1689fffbeb9bf7017b975ea4c22b4dd8ad9b7
OppedRawen <yud2372@gmail.com>
2023-07-02 23:19:52 -0700

    experience redesign

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,7 @@ import {IoCaretForwardOutline} from "react-icons/io5"
 const Experience = () => {
     const experience =[
         {
+          year:"2023",
           title: "Web Development Project Lead",
           company: "IVC Computer Science Club",
           duration: "January 2023- Present",
@@ -16,6 +17,7 @@ club members</li>
           )
         },
         {
+          year:"2023",
           title: "Line Cook",
           company: "Din Tai Fung",
           duration: "Feburary 2023- Present",
@@ -29,6 +31,7 @@ club members</li>
           )
         },
         {
+          year:"2021",
           title: "Assistant Manager",
           company: "Champion's Curry",
           duration: "June 2021- January 2023",
@@ -49,17 +52,34 @@ club members</li>
         <section id="experience" className="max-w-screen-lg m-auto md:pl-20 px-8 py-24 sm:py-32">
         <h2 className="text-3xl py-2 text-gray-500 font-medium text-center">Experience</h2>
         {experience.map((data)=>(
-          <ol>
-            <li className="">
+          <ol className="flex flex-col md:flex-row relative border-l border-stone-300">
+            <li className="mb-10 ml-4">
 
-            <div className=" flex flex-col md:flex-row relative border-l border-stone-300">
+            <div className="absolute mt-1.5 -left-1.5 w-3 h-3 bg-stone-300 rounded-full">
+              
+            </div>
+            <p className="flex flex-wrap flex-row justify-start items-center text-xs md:text-sm">
+              <span className="inline-blox py-1 px-2 mr-4 font-medium text-white bg-gray-600 rounded-md">{data.year}</span>
+              <span className="mr-1 text-base md:text-lg font-medium text-slate-400">{data.title} </span>
+              <span className="text-[#cacaca] ml-1 font-medium text-lg"> @ {data.company}</span>
+            </p>
+            <p className="mt-3 text-xs sm:text-sm font-normal text-stone-400">
+            {data.duration}
+            </p>
+            <p className="my-2 font-normal text-[#cacaca]">
+                {data.description()}
+            </p>
+
+            {/* <div className=" flex flex-col md:flex-row relative border-l border-stone-300">
+            
               <span className="inline-blox py-1 px-2 mr-4 font-medium text-white bg-gray-600 rounded-md">{data.duration}</span>
               <span className="mr-1 text-base md:text-lg font-medium text-slate-400">{data.company}</span>
               <p className="text-[#cacaca] ml-1 font-medium text-lg">{data.title}</p>
               </div>
               <div className="my-2 font-normal text-[#cacaca]">
                 {data.description()}
-              </div>
+              </div> */}
+
             </li>
           </ol>
          
