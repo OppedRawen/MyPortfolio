7eb86065e0a280e81943002cb848d935e6675f7b
OppedRawen <yud2372@gmail.com>
2024-02-26 23:26:56 -0800

    navbar annimation delays

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,12 +14,13 @@ const Navbar = () => {
 
     return(
 
-  <nav className="py-10 mb-12 flex justify-between text-[#cacaca]">
+  <nav className="sticky py-10 mb-12 flex justify-between text-[#cacaca]">
+    <div className="flex justify-between h-full w-full text-sm px-6">
       {/* <h1 className="text-lg font-burtons font-semibold">Portfolio</h1> */}
       <motion.div
       initial={{ opacity: 0}}
       animate={{ opacity: 1}}
-      transition={{ duration: 3, delay: 3}}
+      transition={{ duration: 3, delay: 1.75}}
       viewport={{once:true}}
       >
         <a href="/">
@@ -29,9 +30,9 @@ const Navbar = () => {
       </motion.div>
     
       
-      <div className="flex justify-end text-sm">
-        <ul className="flex items-center">
-          <li className="ml-6 mr-6 font-semibold hover:text-[#54d5bb]">
+        <div>
+        <ul className="hidden md:flex md:items-center list-none">
+        <motion.li initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 2 }} viewport={{ once: true }} className="mr-6">
             <motion.a
               href="#about"
               whileTap={{ scale: 0.95 }}
@@ -39,8 +40,8 @@ const Navbar = () => {
             >
               About Me
             </motion.a>
-          </li>
-          <li className="ml-6 mr-6 font-semibold hover:text-[#54d5bb]">
+          </motion.li>
+          <motion.li initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 2.25 }} viewport={{ once: true }} className="mr-6">
             <motion.a
               href="#experience"
               whileTap={{ scale: 0.95 }}
@@ -48,8 +49,8 @@ const Navbar = () => {
             >
               Experience
             </motion.a>
-          </li>
-          <li className="ml-6 mr-6 font-semibold hover:text-[#54d5bb]">
+          </motion.li>
+          <motion.li initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 2.5 }} viewport={{ once: true }} className="mr-6">
             <motion.a
               href="#projects"
               whileTap={{ scale: 0.95 }}
@@ -57,8 +58,8 @@ const Navbar = () => {
             >
               Projects
             </motion.a>
-          </li>
-          <li className="ml-6 mr-6 font-semibold hover:text-[#54d5bb]">
+          </motion.li>
+          <motion.li initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 2.75 }} viewport={{ once: true }}>
             <motion.a
               href="#contact"
               whileTap={{ scale: 0.95 }}
@@ -66,8 +67,8 @@ const Navbar = () => {
             >
               Contact
             </motion.a>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 3 }} viewport={{ once: true }}>
           <a
               className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
               href="https://drive.google.com/file/d/1CgIuKID96dTHESQNIpPQlI4xXiv6J3iu/view?usp=sharing"
@@ -76,8 +77,9 @@ const Navbar = () => {
             >
               Resume
             </a>
-          </li>
+          </motion.li>
         </ul>
+        </div>
       </div>
     </nav>
     );
